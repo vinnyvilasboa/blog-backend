01c41651b19057a51522d479a51875e50dbca09b
vinnyvilasboa <vinnycesca@gmail.com>
2021-12-15 19:12:30 -0500

    port working and env is set up

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+
+//app
+const app = express()
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+//cors
+app.use(cors())
+
+
+//routes
+app.get('/api', (req,res) => {
+    res.json({time: Date().toString()})
+})
+
+//anytime the app receives a request to the API we will respond with Date in JSON format
+
+//port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
