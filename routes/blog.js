204db2cf83dec11fdb69eaf2f5f6e931d5fa58ab
vinnyvilasboa <vinnycesca@gmail.com>
2021-12-20 11:38:58 -0500

    need to fix error in controller

---
+++ routes/blog.js
@@ -0,0 +1,10 @@
+const express =  require('express')
+const router = express.Router()
+const {time} = './controllers/blog'
+
+
+router.get('/', time)
+
+module.exports = router;
+
+//so whatever happens here is called into the server and used as middleware.
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose');
 require('dotenv').config()
+const blogRoutes = require('./routes/blog')
 
 
 //app
 const app = express()
+// mongoose.connect(process.env.DATABASE, {useNewUrlParser:true, useCreateIndex: true, useFindAndModify: false}). then(()=> console.log('DB connected'));
+//database
+mongoose.connect(process.env.DATABASE_CLOUD).then(()=> console.log('DB connected'));
 
 //middlewares
 app.use(morgan('dev'))
@@ -17,13 +22,9 @@ app.use(cookieParser())
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({origin: `${process.env.CLIENT_URL}`}))
 }
-
-
-
-//routes
-app.get('/api', (req,res) => {
-    res.json({time: Date().toString()})
-})
+//routes middleware
+app.use('/api',blogRoutes)  
+//it's the same as having it all written out there but we put it somewhere else for clean code purposes
 
 //anytime the app receives a request to the API we will respond with Date in JSON format
 
