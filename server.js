75cb4c21878cd031134ed5982e8cd71aa328f91b
vinnyvilasboa <vinnycesca@gmail.com>
2022-01-19 13:04:03 -0500

    review

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express') //-
+const morgan = require('morgan') 
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
