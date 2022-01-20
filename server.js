e25bacf5b29921e73582365daa535094a84bfc4b
vinnyvilasboa <vinnycesca@gmail.com>
2022-01-20 10:16:10 -0500

    cleaning up code

--- server.js
+++ server.js
@@ -6,34 +6,42 @@ const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-//bring in routes 
+//////////////////////////ROUTES//////////////////////////////////////
+
 const blogRoutes = require('./routes/blog')
 const authRoutes = require('./routes/auth')
-//app
+
+//////////////////////////APP//////////////////////////////////////
 const app = express()
-// mongoose.connect(process.env.DATABASE, {useNewUrlParser:true, useCreateIndex: true, useFindAndModify: false}). then(()=> console.log('DB connected'));
-//database
-// mongoose.connect(process.env.DATABASE_CLOUD).then(()=> console.log('DB connected'));
+
+
+
+//////////////////////////DATABASE//////////////////////////////////////
 mongoose
   .connect(process.env.DATABASE_CLOUD, {})
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB Error => ", err));
-//middlewares
+
+
+////////////////////////////MIDDLEWARE////////////////////////////////////
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
-//cors
+//////////////////////////CORS//////////////////////////////////////
+/*If environment is still in dev phase then use CORS with the origin being the client URL*/
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({origin: `${process.env.CLIENT_URL}`}))
 }
-//routes middleware
+//////////////////////////Routes Middleware//////////////////////////////////////
+
 app.use('/api',blogRoutes)  
 app.use('/api',authRoutes)  
 //it's the same as having it all written out there but we put it somewhere else for clean code purposes
 
 //anytime the app receives a request to the API we will respond with Date in JSON format
 
-//port
+//////////////////////////PORT//////////////////////////////////////
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
