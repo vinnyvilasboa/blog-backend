24092aee793332baf6c625e15e1045acdb293ccf
vinnyvilasboa <vinnycesca@gmail.com>
2022-01-25 13:08:41 -0500

    new user working with db

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,16 +3,33 @@ const shortId = require('shortid')
 
 
 exports.signup = (req, res) => {
+    //FIND IF NEW USER EXISTS AGAINST DB
     User.findOne({ email: req.body.email }).exec((err, user) => {
         if (user) {
             return res.status(400).json({
                 error: 'Email is taken'
             })
         }
-        const {name, email, password} = req.body
+
+        //OTHERWISE CREATE NEW USER
+        const { name, email, password } = req.body
         let username = shortId.generate()
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+        let newUser = new User({ name, email, password, profile, username })
+        newUser.save((err, success) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.json({
+                message: 'Sign-Up successful - Please Sign-In.'
+                
+            })
+        })
     })
-}   
+}
 
 // const { name, email, password } = req.body
 //     res.json({
