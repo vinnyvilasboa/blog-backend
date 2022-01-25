d1e801aec8dd7666b0bfcec0340f52d9078b57ec
vinnyvilasboa <vinnycesca@gmail.com>
2022-01-25 12:05:14 -0500

    working on new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,20 @@
+const User = require('../models/user');
+const shortId = require('shortid')
+
+
 exports.signup = (req, res) => {
-    const { name, email, password } = req.body
-    res.json({
-        user: { name, email, password }
+    User.findOne({ email: req.body.email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is taken'
+            })
+        }
+        const {name, email, password} = req.body
+        let username = shortId.generate()
     })
 }   
+
+// const { name, email, password } = req.body
+//     res.json({
+//         user: { name, email, password }
+//     })
